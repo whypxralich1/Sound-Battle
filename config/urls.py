2c4c81a907ed100869b4f3069afea0af45b853dc
whypxralich1 <maxgliantsev@gmail.com>
2026-04-26 23:40:29 +0300

    Add feedback

--- config/urls.py
+++ config/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from pages.views import index, about, track_detail
+from pages import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index, name='home'),
-    path('about/', about, name='about'),
-    path('track/<int:pk>/', track_detail, name='track_detail'),
+    path('', views.index, name='home'),
+    path('about/', views.about, name='about'),
+    path('track/<int:pk>/', views.track_detail, name='track_detail'),
+    path('contact/', views.contact_view, name='contact'),
 ]
